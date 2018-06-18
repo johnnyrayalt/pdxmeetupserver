dbd2022f93f306524aed380e6ca1c9d2eaffe860
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-18 16:11:32 -0700

    heroku credentials

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,13 +17,16 @@ public class App {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
-        
+
         Sql2oUserDao userDao;
         Connection conn;
         Gson gson = new Gson();
 
-        String connectionString = "postgresql://localhost:5432/pdxmeetups";
-        Sql2o sql2o = new Sql2o(connectionString, null, null);
+        //Local PostgresDB for Test Environment
+//        String connectionString = "postgresql://localhost:5432/pdxmeetups";
+        //Heroku Credentials
+        String connectionString = "postgresql://uvwijeyphjojeu:0c4765d4acb8fc63391654eee96e7470e53747450fae08f30e91412c012a577f@ec2-23-21-238-28.compute-1.amazonaws.com:5432/d32rom86rntfh9";
+        Sql2o sql2o = new Sql2o(connectionString, "uvwijeyphjojeu", "0c4765d4acb8fc63391654eee96e7470e53747450fae08f30e91412c012a577f");
         userDao = new Sql2oUserDao(sql2o);
         conn = sql2o.open();
 
