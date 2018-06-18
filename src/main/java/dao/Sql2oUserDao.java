ae68339be68e3bf1da94533e48972995dd45ff22
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-18 13:59:47 -0700

    get all user method and test

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -5,6 +5,8 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
+import java.util.List;
+
 public class Sql2oUserDao implements UserDao {
     private final Sql2o sql2o;
 
@@ -21,6 +23,14 @@ public class Sql2oUserDao implements UserDao {
             user.setId(id);
         } catch (Sql2oException ex) {
             System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM users")
+                    .executeAndFetch(User.class);
         }
     }
 
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,13 +2,15 @@ package dao;
 
 import models.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     //Create
     void add(User user);
 
     //Read
-
+    List<User> getAll();
 
     //Update
 
