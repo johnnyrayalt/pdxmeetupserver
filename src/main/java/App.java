b12e3538fed5e3b0ac7eef78761f83e5f099f618
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-18 13:37:15 -0700

    routing for adding a new user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,32 @@
+import com.google.gson.Gson;
+import dao.Sql2oUserDao;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
+
 public class App {
     public static void main(String[] args) {
 
+        Sql2oUserDao userDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        staticFileLocation("/public");
+        String connectionString = "jbbc:postgresql://localhost:5432/pdxmeetups";
+        Sql2o sql2o = new Sql2o(connectionString, null, null);
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+
+
+        post("api/user/new", "application/json", (request, response) -> {
+            User user = gson.fromJson(request.body(), User.class);
+            userDao.add(user);
+            response.status(201);
+            response.type("application/json");
+            return gson.toJson(user);
+        });
     }
 }
