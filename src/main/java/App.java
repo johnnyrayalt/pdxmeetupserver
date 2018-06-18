99b10a6c8ae52653fca5df620b1596e123b1ebcb
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-18 16:57:01 -0700

    heroku update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
-import com.google.gson.Gson;
-import dao.Sql2oUserDao;
 import models.User;
+import dao.Sql2oUserDao;
+import com.google.gson.Gson;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import static spark.Spark.*;
@@ -24,8 +24,10 @@ public class App {
 
         //Local PostgresDB for Test Environment
 //        String connectionString = "postgresql://localhost:5432/pdxmeetups";
+
         //Heroku Credentials
         String connectionString = "postgresql://uvwijeyphjojeu:0c4765d4acb8fc63391654eee96e7470e53747450fae08f30e91412c012a577f@ec2-23-21-238-28.compute-1.amazonaws.com:5432/d32rom86rntfh9";
+
         Sql2o sql2o = new Sql2o(connectionString, "uvwijeyphjojeu", "0c4765d4acb8fc63391654eee96e7470e53747450fae08f30e91412c012a577f");
         userDao = new Sql2oUserDao(sql2o);
         conn = sql2o.open();
