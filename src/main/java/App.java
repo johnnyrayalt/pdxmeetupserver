b0d146f9a2200d55b89cf0ddd0d9dcf8ecd81850
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-18 15:20:27 -0700

    restful routing to update user name and or photo by id

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,9 +4,9 @@ import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
-import static spark.Spark.post;
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -22,24 +22,36 @@ public class App {
         conn = sql2o.open();
 
 
+        // Add a new User
         post("/api/users/new", "application/json", (request, response) -> {
             User user = gson.fromJson(request.body(), User.class);
             userDao.add(user);
             response.status(201);
-            response.type("application/json");
             return gson.toJson(user);
         });
 
+        // Get all Users
         get("/api/users", "application/json", (request, response) -> {
-            response.type("application/json");
             return gson.toJson(userDao.getAll());
         });
 
+        // Get individual User
         get("/api/users/:id", "application/json", (request, response) -> {
-            response.type("application/json");
             int userId = Integer.parseInt(request.params("id"));
-            response.type("application/json");
             return gson.toJson(userDao.findById(userId));
+        });
+
+        // Update User
+        post("/api/users/:id", "application/json", (request, response) -> {
+            int userId = Integer.parseInt(request.params("id"));
+            User updatedUser = gson.fromJson(request.body(), User.class);
+            userDao.update(userId, updatedUser.getName(), updatedUser.getPhoto());
+            return gson.toJson(userDao.findById(userId));
+        });
+
+        //FILTERS
+        after((req, res) ->{
+            res.type("application/json");
         });
     }
 }
