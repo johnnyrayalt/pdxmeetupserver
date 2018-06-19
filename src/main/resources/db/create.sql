9c76d020cbd2e07cd68c466ecc8c008b5b14d3b3
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-19 08:27:35 -0700

    heroku config updates

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,40 +1,23 @@
 CREATE DATABASE pdxmeetups;
 \c pdxmeetups;
 
--- ---
--- Table 'users'
---
--- ---
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR,
   photo VARCHAR
 );
 
--- ---
--- Table 'events'
---
--- ---
 
 CREATE TABLE events (
   id SERIAL PRIMARY KEY
 );
 
--- ---
--- Table 'topics'
---
--- ---
 
 CREATE TABLE topics (
   id SERIAL PRIMARY KEY,
   name VARCHAR
 );
 
--- ---
--- Table 'users_events'
---
--- ---
 
 CREATE TABLE users_events (
   id SERIAL PRIMARY KEY,
@@ -42,10 +25,6 @@ CREATE TABLE users_events (
   eventid INTEGER
 );
 
--- ---
--- Table 'users_topics'
---
--- ---
 
 CREATE TABLE users_topics (
   id SERIAL PRIMARY KEY,
@@ -53,10 +32,6 @@ CREATE TABLE users_topics (
   topicid INTEGER
 );
 
--- ---
--- Table 'topics_events'
---
--- ---
 
 CREATE TABLE topics_events (
   id SERIAL PRIMARY KEY,
