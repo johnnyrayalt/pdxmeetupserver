d921b772e61a4d7eef37fb95075aeeabd3ddb70e
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-19 10:35:51 -0700

    events object pojo

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,29 @@
 package models;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Event {
+    private List<Event> eventList;
+
+    public Event(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    public List<Event> getEventList() { return eventList; }
+    public void setEventList(List<Event> eventList) { this.eventList = eventList; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(eventList, event.eventList);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(eventList);
+    }
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,5 +1,16 @@
+package models;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
 
+    @Test
+    public void getEventList() {
+    }
+
+    @Test
+    public void setEventList() {
+    }
 }
