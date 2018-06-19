2ddeb8defcf50a34119d7ddf44ec0e3c096f01af
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-19 11:01:13 -0700

    workign tests for Event POJO

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -2,15 +2,33 @@ package models;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
 
     @Test
-    public void getEventList() {
+    public void eventObjectInstantiatesCorrectly() throws Exception {
+        Event event = setNewEvent();
+        assertTrue(event instanceof Event);
     }
 
     @Test
-    public void setEventList() {
+    public void getMeetUpApiId_returnsIDFromMeetUpApi() throws Exception {
+        Event event = setNewEvent();
+        assertEquals(1, event.getMeetUpApiId());
+    }
+
+    @Test
+    public void getId_returnsPdxMeetUpDbId() throws Exception {
+        Event event = setNewEvent();
+        event.setId(1);
+        assertEquals(1, event.getId());
+    }
+
+    //Helpers
+    public Event setNewEvent() {
+        return new Event(1);
     }
 }
