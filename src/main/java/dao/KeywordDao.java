544310e17072a4f8f4be02c260891e794c18d00f
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-20 10:02:53 -0700

    sql2o implimentation stucture

---
+++ src/main/java/dao/KeywordDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import models.Keyword;
+
+import java.util.List;
+
+public interface KeywordDao {
+    //Create
+//    void add(Keyword keyword);
+
+    //Read
+//    List<Keyword> getAll();
+//    Keyword findbyId(int id);
+
+    //Delete
+//    void deleteById(int id);
+//    void clearAllKeywords();
+}
