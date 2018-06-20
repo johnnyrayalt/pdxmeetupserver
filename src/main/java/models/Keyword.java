99b9941308f959d6eec56cbe8460cb1fba50882d
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-20 09:46:38 -0700

    Keyword POJO

---
+++ src/main/java/models/Keyword.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.Objects;
+
+public class Keyword {
+    private String keyword;
+    private int id;
+
+
+    private Keyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getKeyword() { return keyword; }
+    public void setKeyword(String keyword) { this.keyword = keyword; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Keyword keyword1 = (Keyword) o;
+        return id == keyword1.id &&
+                Objects.equals(keyword, keyword1.keyword);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(keyword, id);
+    }
+}
