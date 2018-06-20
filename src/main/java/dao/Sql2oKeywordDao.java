1758edf31c75f49ffc599d9c0d48b29b64e0509a
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-20 10:02:53 -0700

    sql2o implimentation stucture

---
+++ src/main/java/dao/Sql2oKeywordDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import org.sql2o.Sql2o;
+
+public class Sql2oKeywordDao implements KeywordDao {
+    private final Sql2o sql2o;
+
+    public Sql2oKeywordDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+
+
+}
